babdf35e5e3d4a093a8c901e6d3d20794bbe8a21
Nac <teampavilion@gmail.com>
2017-01-04 08:29:22 +0100

    Add project files.

---
+++ SHDN.Essentials/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHDN.Essentials
+{
+    public class Class1
+    {
+    }
+}
